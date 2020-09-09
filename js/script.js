a6fecfa2b143fbe1538a9bf9a9cf6c5eb6731fc1
Riccardo Colombo <rikyregna@gmail.com>
2020-09-09 18:23:47 +0200

    4. renameListItem() => ajaxRenameCall()

--- js/script.js
+++ js/script.js
@@ -10,9 +10,57 @@ Utilizzare l’ API di esempio http://157.230.17.132:3007/todos
 
 
 $(function () {
+	// acquisisco la lista dall API
 	getList();
+	// rimozione elemento al click del tasto .delete-item
 	$('#list').on('click', '.delete-item', deleteListItem);
+	// aggiunta del testo al click di #add-element-btn
 	$('#add-element-btn').click(addListItem);
+
+	// rinomino un elemento al click di .item
+	$('#list').on('click', '.item', renameListItem);
+
+	function renameListItem() {
+
+		$(this).toggle();
+		$(this).next().toggle();
+
+		$('#list').on('keydown', '.change-element', function (e) {
+			// valore acquisito da input
+			let inputText = $(this).val();
+			// id corrente
+			let thisId = $(this).data('id');
+			// show element al termine
+			let $showElement = $(`.item[data-id="${thisId}"`);
+			if (e.which == 13 && e.keyCode == 13 && inputText) {
+				$(this).val('');
+				$(this).toggle();
+				$showElement.toggle();
+
+				ajaxRenameCall(thisId, inputText);
+
+
+				function ajaxRenameCall(id, newText) {
+					console.log(id);
+					$.ajax({
+						url: `http://157.230.17.132:3007/todos/${id}`,
+						method: 'PUT',
+						data: {
+							text: newText
+						},
+						success: function () {
+							$('#list').html('');
+							getList();
+						},
+						error: function () {
+							alert('errore');
+						}
+					})
+				}
+			}
+		});
+	}
+
 });
 
 
@@ -33,10 +81,9 @@ function printList(data) {
 	let source = $("#entry-template").html();
 	let template = Handlebars.compile(source);
 	data.forEach(element => {
-		console.log(element);
 		let context = element;
 		let html = template(context);
-		$('#list').prepend(html);
+		$('#list').append(html);
 	});
 }
 
@@ -46,7 +93,6 @@ function deleteListItem() {
 		url: `http://157.230.17.132:3007/todos/${$thisElement}`,
 		method: 'DELETE',
 		success: function (risposta) {
-			console.log(risposta);
 			$('#list').html('');
 			getList();
 		},
